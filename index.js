72ded09eb402327067a823cabb2fd4848457ed76
Varun Rewadi <varunrewadi.vr005@gmail.com>
2025-03-28 13:52:00 +0530

    fixed the Projects section

--- index.js
+++ index.js
@@ -8,56 +8,69 @@ const resumeData = require('./resumeData.json');
 
 let blueColor = chalk.blue.bold;
 let redColor = chalk.red.bold;
+let yellowColor = chalk.yellow.bold;
 
 var prompts = {
     type: "list",
     name: "option",
     message: "What would you like to know about me?",
     choices: [...Object.keys(resumeData), "Exit"]
-  };
+};
 
-(function(){
-    clear();    
+(function () {
+    clear();
     console.log(
         chalk.cyan.bold(
-            figlet.textSync('      Varun Rewadi     ',{
-            horizontalLayout:'full',
-        }))
+            figlet.textSync('      Varun Rewadi     ', {
+                horizontalLayout: 'full',
+            }))
     );
-    console.log("\n")
-    console.log(chalk.red.bold(" \u{1F916}  Full Stack Developer") + " \n\n + Believes in possibilities and always trying to look at the positive, brighter side of the story. \n\n • Have good experience in fullstack web development. \n • Have e2e knowledge of IoT applications including edge computing and cloud.")
-    console.log("\n")    
-    menu();  
+    console.log("\n");
+    console.log(chalk.red.bold(" 🤖 Full Stack Developer") + 
+        " \n\n + Believes in possibilities and always trying to look at the positive, brighter side of the story. \n\n • Have good experience in fullstack web development. \n • Have e2e knowledge of IoT applications including edge computing and cloud.");
+    console.log("\n");
+    menu();
 })();
 
-function menu(){
-    inquirer.prompt(prompts).then(choice=>{
+function menu() {
+    inquirer.prompt(prompts).then(choice => {
         let selectedOption = choice.option;
-        if(selectedOption==="Exit"){
+        if (selectedOption === "Exit") {
             console.log(redColor("\nThank you for showing interest. Have a great day !"));
             return;
         }
-        console.log('\n====================================\n');
-        
-        resumeData[`${selectedOption}`].forEach(data => {
-            console.log("\u{1F538}   "+ blueColor(data));
-        });
-        console.log('\n====================================\n');
-        
-        inquirer.prompt({
-        type: "list",
-        name: "option",
-        message: "Go back or Exit?",
-        choices: ["Back", "Exit"]
-        })
-        .then(choice => {
-            if (choice.option == "Back") {
-                menu();
-            } else {
-                console.log(redColor("\nThank you for showing interest. Have a great day !"));
-                return;
-            }
-        });
 
-    })
+        console.log('\n====================================\n');
+
+        if (selectedOption === "Projects") {
+            resumeData["Projects"].forEach((project, index) => {
+                console.log(`${yellowColor(`🔹 ${project.name}`)}`);
+                console.log(`   ${blueColor("➥ Description:")} ${project.description}`);
+                console.log(`   ${blueColor("🔧 Tech Stack:")} ${project.techStack.join(", ")}`);
+                console.log("\n");
+            });
+        } else {
+            resumeData[selectedOption].forEach(data => {
+                console.log("🔸 " + blueColor(data));
+            });
+        }
+
+        console.log('\n====================================\n');
+
+        inquirer.prompt({
+            type: "list",
+            name: "option",
+            message: "Go back or Exit?",
+            choices: ["Back", "Exit"]
+        })
+            .then(choice => {
+                if (choice.option == "Back") {
+                    menu();
+                } else {
+                    console.log(redColor("\nThank you for showing interest. Have a great day !"));
+                    return;
+                }
+            });
+
+    });
 }
